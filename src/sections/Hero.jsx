de4ab00fd7d648606b7b66d1102db3c19271dd28
Ashish-Mohapatra478846 <ashishmohapatrapopun@gmail.com>
2025-07-01 17:52:13 +0530

    First commit

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
 
       <h1 className='mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold'>
         <span className='xl:bg-white xl:whitespace-nowrap relative z-10 pr-10'>
-          The New Arrival
+          Hello My Name is Ashish
         </span>
         <br />
         <span className='text-coral-red inline-block mt-3'>Nike</span> Shoes
